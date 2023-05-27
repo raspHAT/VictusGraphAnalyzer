144b5905e71362fb581dbe527e6cf39abca76378
rasphat <info@rasphat.com>
2023-05-27 23:11:54 +0200

    VictusGraphAnalyzer V0.0.9

--- src/main/java/com/rasphat/zipExtractor/ZipHandler.java
+++ src/main/java/com/rasphat/zipExtractor/ZipHandler.java
@@ -5,7 +5,27 @@ import net.lingala.zip4j.exception.ZipException;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * The ZipHandler interface defines the operations that must be implemented
+ * by classes that handle specific types of ZIP files.
+ */
 public interface ZipHandler {
-    void handleZip(File File, String path, char[] password) throws IOException;
+
+    /**
+     * Handles a given ZIP file.
+     *
+     * @param file     the ZIP file to be handled.
+     * @param path     the path where the ZIP file should be extracted.
+     * @param password the password to be used to decrypt the ZIP file, if encrypted.
+     * @throws IOException if an I/O error occurs.
+     */
+    void handleZip(File file, String path, char[] password) throws IOException;
+
+    /**
+     * Handles a given ZipException.
+     * This method should implement appropriate actions to be taken when a ZipException occurs.
+     *
+     * @param e the ZipException to be handled.
+     */
     void handleException(ZipException e);
 }
